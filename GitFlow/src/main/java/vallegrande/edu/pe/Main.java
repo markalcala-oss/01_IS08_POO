c71a39a965417e959b3bcd2e293ce2daf11b1e5f
MarkAlcalaVG <mark.alcala@vallegrande.edu.pe>
2026-05-13 12:06:01 -0500

    feat: crud de productos funcional con validaciones y conexion exitosa

--- GitFlow/src/main/java/vallegrande/edu/pe/Main.java
+++ GitFlow/src/main/java/vallegrande/edu/pe/Main.java
@@ -1,17 +1,24 @@
 package vallegrande.edu.pe;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import vallegrande.edu.pe.vista.FrmPrincipal;
+import vallegrande.edu.pe.controlador.ProductoController;
+import javax.swing.UIManager;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // 1. Ponerle estilo de Windows (Mejora visual requerida)
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        // 2. Iniciar la Vista y el Controlador
+        // Esto une las capas del MVC
+        FrmPrincipal vista = new FrmPrincipal();
+        new ProductoController(vista);
+
+        // 3. Mostrar la ventana
+        vista.setVisible(true);
     }
 }
